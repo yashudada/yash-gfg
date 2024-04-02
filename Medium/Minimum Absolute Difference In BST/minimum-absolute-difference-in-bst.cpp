7be6969efe2e367c583bee99141d2f724a8c4a5c
shreyash_2300 <140648698+yashudada@users.noreply.github.com>
2024-04-02 22:50:28 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Minimum Absolute Difference In BST/minimum-absolute-difference-in-bst.cpp
@@ -0,0 +1,150 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+  int data;
+  struct Node *left;
+  struct Node *right;
+};
+// Utility function to create a new Tree Node
+Node *newNode(int val)
+{
+  Node *temp = new Node;
+  temp->data = val;
+  temp->left = NULL;
+  temp->right = NULL;
+
+  return temp;
+}
+// Function to Build Tree
+Node *buildTree(string str)
+{
+  // Corner Case
+  if (str.length() == 0 || str[0] == 'N')
+    return NULL;
+
+  // Creating vector of strings from input
+  // string after spliting by space
+  vector<string> ip;
+
+  istringstream iss(str);
+  for (string str; iss >> str;)
+    ip.push_back(str);
+
+  // Create the root of the tree
+  Node *root = newNode(stoi(ip[0]));
+
+  // Push the root to the queue
+  queue<Node *> queue;
+  queue.push(root);
+
+  // Starting from the second element
+  int i = 1;
+  while (!queue.empty() && i < ip.size())
+  {
+
+    // Get and remove the front of the queue
+    Node *currNode = queue.front();
+    queue.pop();
+
+    // Get the current node's value from the string
+    string currVal = ip[i];
+
+    // If the left child is not null
+    if (currVal != "N")
+    {
+
+      // Create the left child for the current node
+      currNode->left = newNode(stoi(currVal));
+
+      // Push it to the queue
+      queue.push(currNode->left);
+    }
+
+    // For the right child
+    i++;
+    if (i >= ip.size())
+      break;
+    currVal = ip[i];
+
+    // If the right child is not null
+    if (currVal != "N")
+    {
+
+      // Create the right child for the current node
+      currNode->right = newNode(stoi(currVal));
+
+      // Push it to the queue
+      queue.push(currNode->right);
+    }
+    i++;
+  }
+
+  return root;
+}
+
+
+// } Driver Code Ends
+/*The Node structure is defined as
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+};
+*/
+
+class Solution
+{
+    public:
+     pair<int,int> help(Node* root, int& ans){
+        if(root->left==NULL&&root->right==NULL){
+            return {root->data,root->data};
+        }
+        pair<int,int>left={-1e9,1e9},right={-1e9,1e9};
+        if(root->left)left=help(root->left,ans);
+        if(root->right)right=help(root->right,ans);
+        if(left.second!=1e9){
+            ans=min(ans,root->data-left.second);
+        }
+        if(right.first>0){
+            ans=min(ans,right.first-root->data);
+        }
+        return {left.first==-1e9?root->data:left.first,right.second==1e9?root->data:right.second};
+    }
+    int absolute_diff(Node *root)
+    {
+        //Your code here
+        int ans=INT_MAX;
+        help(root,ans);
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+
+  int t;
+  scanf("%d ", &t);
+  while (t--)
+  {
+    string s;
+    getline(cin, s);
+    Node *root = buildTree(s);
+    // getline(cin, s);
+    if (root == NULL)
+      continue;
+    if (root->left == NULL and root->right == NULL)
+      continue;
+      Solution ob;
+    cout << ob.absolute_diff(root) << '\n';
+    // cout << s << '\n';
+  }
+  return 1;
+}
+// } Driver Code Ends
